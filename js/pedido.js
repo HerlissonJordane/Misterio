a0ce3286eb0ea853883ee442b99d91ebdd68cd86
HerlissonJordane <128053595+SIPAKSistemas@users.noreply.github.com>
2023-05-02 13:26:43 -0300

    Ajustando a bagunça

--- js/pedido.js
+++ js/pedido.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const moveButton  = document.getElementById("btn-nao");
 const yesButtom = document.getElementById("btn-sim");
 
@@ -30,37 +29,4 @@ yesButtom.addEventListener("click", () =>{
 
 function aceitou(){
     window.location.href = "fotos.html"
-=======
-const moveButton  = document.getElementById("btn-nao");
-const yesButtom = document.getElementById("btn-sim");
-
-console.log(yesButtom);
-
-
-//MUDA DE LUGAR SE CLICAR NO BOTÃO
-moveButton.addEventListener("click", () => { 
-    randomButton();    
-});
-
-
-//FAZ MNUDAR DE LUGAR SE O MOUSE PASSAR POR CIMA
-moveButton.onmouseover = function() {randomButton()};
-
-
-//CRIA A NOVA POSIÇÃO DO BOTÃO
-function randomButton(){
-    var vertical = Math.floor(Math.random() * 400);
-    var horizontal = Math.floor(Math.random() * 400);
-    moveButton.style.position = "absolute";
-    moveButton.style.left = vertical + "px";
-    moveButton.style.top = horizontal + "px";
-}
-
-yesButtom.addEventListener("click", () =>{
-    aceitou();
-})
-
-function aceitou(){
-    window.location.href = "fotos.html"
->>>>>>> 440f4a2 (Version Final)
 }
